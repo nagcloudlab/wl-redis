af0b852ba4bc91c899db9f4fbc2ed6c7962cf797
nagcloudlab <67089749+nagcloudlab@users.noreply.github.com>
2024-11-11 15:53:17 +0530

    day-1 commit

--- demo-service/src/main/java/com/example/demo_service/DemoServiceApplication.java
+++ demo-service/src/main/java/com/example/demo_service/DemoServiceApplication.java
@@ -3,12 +3,15 @@ package com.example.demo_service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.Cacheable;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
+@EnableCaching
 public class DemoServiceApplication {
 
 	//String reqCount = "0";
@@ -22,7 +25,7 @@ public class DemoServiceApplication {
 		// count++;
 		// reqCount = Integer.toString(count);
 		// return "Hello from Demo Service. Request Count: " + reqCount;
-		
+
 		// String reqCount = redisTemplate.opsForValue().get("reqCount"); // read // GET reqCount
 		// if (reqCount == null) {
 		// 	reqCount = "0";
@@ -38,6 +41,15 @@ public class DemoServiceApplication {
 		return "Hello from Demo Service. Request Count: " + reqCount;
 
 	}
+	
+
+	@GetMapping("/data")
+	@Cacheable("dataCache")
+    public String getData() throws InterruptedException {
+        // Simulate a slow data source (e.g., database call)
+        Thread.sleep(500); // 500ms delay
+        return "Sample Data"+Math.random();
+    }
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoServiceApplication.class, args);
